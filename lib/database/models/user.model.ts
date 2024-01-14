c9a40b3990179e843626e919978a076d8275a293
ImpactFurax <bortjean@yahoo.fr>
2024-01-14 15:02:10 +0100

    update

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -4,8 +4,6 @@ const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: {type: String, required: true, unique: true },
   username: {type: String, required: true, unique: true },
-  firstName: {type: String },
-  lastName: {type: String },
   photo: { type: String, required: true },
 })
 
--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,5 @@
 export type CreateUserParams = {
   clerkId: string
-  firstName: string
-  lastName: string
   username: string
   email: string
   photo: string
