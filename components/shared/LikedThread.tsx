9f2c2a52c658a7ece4cbf2e8f29b2fc46a92e756
ImpactFurax <bortjean@yahoo.fr>
2024-01-21 12:59:27 +0100

    update like

--- components/shared/LikedThread.tsx
+++ components/shared/LikedThread.tsx
@@ -3,6 +3,7 @@ import { alreadyLike, userLikeThread } from "@/lib/actions/like.actions";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
+import { useSession } from "@clerk/nextjs";
 
 type likedThreadProps = {
   threadId: string;
@@ -10,18 +11,25 @@ type likedThreadProps = {
 }
 
 const LikedThread = ({ threadId, userId }: likedThreadProps) => {
+  const { isSignedIn } = useSession();
   const [isLiked, setIsLiked] = useState(false);
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
   useEffect(() => {
     const isAlreadyLike = async () => {
-      const liked = await alreadyLike({ threadId, userId });
-      if (liked) {
-        setIsLiked(true);
+      if (isSignedIn) {
+        try {
+          const liked = await alreadyLike({ threadId, userId });
+          if (liked) {
+            setIsLiked(true);
+          }
+        } catch (error) {
+          console.error(error);
+        }
       }
     }
     isAlreadyLike();
-  })
+  }, [isSignedIn, threadId, userId])
 
   async function likedButton(isLiked: boolean) {
     try {
@@ -38,13 +46,17 @@ const LikedThread = ({ threadId, userId }: likedThreadProps) => {
     }
   }
   return (
-    <Button className={`bg-zinc-800 h-fit w-fit p-1 rounded-md cursor-pointer hover:bg-zinc-700 ${isButtonDisabled ? 'opacity-50 pointer-events-none' : ''}`} onClick={() => likedButton(isLiked)}>
-      {isLiked ? (
-        <Image src="/assets/icons/like-yes.png" alt="like-yes" width={30} height={30} onClick={() => setIsLiked(false)} />
-      ) : (
-        <Image src="/assets/icons/like-none.png" alt="like-none" width={30} height={30} onClick={() => setIsLiked(true)} />
-      )}
-    </Button>
+    <>
+      {isSignedIn &&
+        <Button className={`bg-zinc-800 h-fit w-fit p-1 rounded-md cursor-pointer hover:bg-zinc-700 ${isButtonDisabled ? 'opacity-50 pointer-events-none' : ''}`} onClick={() => likedButton(isLiked)}>
+          {isLiked ? (
+            <Image src="/assets/icons/like-yes.png" alt="like-yes" width={30} height={30} onClick={() => setIsLiked(false)} />
+          ) : (
+            <Image src="/assets/icons/like-none.png" alt="like-none" width={30} height={30} onClick={() => setIsLiked(true)} />
+          )}
+        </Button>
+      }
+    </>
   )
 }
 
